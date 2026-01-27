46386daa453a067db108a449d3d9c9af6d8db6e1
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2026-01-27 18:49:42 +1100

    :feat: new: Complete nav for mobile devices

--- src/components/header/nav_mobile_menu.tsx
+++ src/components/header/nav_mobile_menu.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { NAV_ITEMS } from "./nav_items";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const NavMobileMenu = () => {
-  return (
-    <div className='h-[calc(100vh-5rem)] w-screen z-10 bg-background'>
-        Nav mobile menu
-    </div>
-  )
+    return (
+        <div className='h-[calc(100vh-5rem)] w-screen z-10 bg-background'>
+            <ul className='w-full flex flex-col gap-3 text-black'>
+                {
+                    NAV_ITEMS.map(({ id, label, href }) => {
+                        return (
+                            <li key={id}>
+                                <Button variant='link' size='lg' asChild>
+                                    <Link className='text-lg font-extrabold' href={href}>{label}</Link>
+                                </Button>
+
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default NavMobileMenu
